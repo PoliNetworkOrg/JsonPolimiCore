b373e90b8f6ab82fc501378504ed6d5791458036
Eliaxie <elia.maggioni.gm@gmail.com>
2021-10-18 20:19:14 +0200

    core

---
+++ JsonPolimi_Core_nf/Tipi/ParametriCondivisi.cs
@@ -0,0 +1,9 @@
+﻿namespace JsonPolimi_Core_nf.Tipi
+{
+    public class ParametriCondivisi
+    {
+        public InfoManifesto infoManifesto = null;
+        public int? anno = null;
+        public string pianostudi2 = null;
+    }
+}
