4d8e0eeef8a7f65ec05ac2e293ed55147752553a
github.nouser@nouser.github.com <github.nouser@nouser.github.com>
2023-02-10 00:00:53 +0100

    Update 297aec77-b270-4ea3-9023-d81a7305dedc.vsidx, 353f774b-8624-4c38-8d2b-714111587223.vsidx, and 109 more files...

---
+++ TestJsonCore/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using JsonPolimi_Core_nf.Tipi;
+
+Console.WriteLine("Hello, World!");
+
+
+;
+//var result = JsonPolimi_Core_nf.Tipi.Gruppo.CheckSeIlLinkVa2_Telegram("https://t.me/joinchat/aY_y5vie7GU2MDNk", default, null);
+;
+
+
+;
+ParametriFunzione parametriFunzione = new();
+
+ListaGruppo x = new ListaGruppo();
+x.Add(new Gruppo() { IdLink = "aY_y5vie7GU2MDNk", Platform = "TG" }, true);
+x.CheckSeILinkVanno2(null, 10, true, 10);
+;
